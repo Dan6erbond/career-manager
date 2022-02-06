bfe84db532dbd9ba028e6eba04910d881f0e71cb
RaviAnand Mohabir <moravrav@gmail.com>
2022-02-06 02:50:05 +0100

    ✨ Remove Password Field from User Serializer and Update Hyperlinks

--- api/career_manager/serializers.py
+++ api/career_manager/serializers.py
@@ -6,12 +6,31 @@ from rest_framework import serializers
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    profile = serializers.PrimaryKeyRelatedField(read_only=True)
-    work_experiences = serializers.HyperlinkedRelatedField(view_name="workexperience-detail", many=True, read_only=True)
+    profile = serializers.HyperlinkedRelatedField(
+        view_name="userprofile-detail", read_only=True
+    )
+    work_experiences = serializers.HyperlinkedRelatedField(
+        view_name="workexperience-detail", many=True, read_only=True
+    )
 
     class Meta:
         model = User
-        fields = "__all__"
+        fields = [
+            "url",
+            "profile",
+            "work_experiences",
+            "last_login",
+            "is_superuser",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "is_staff",
+            "is_active",
+            "date_joined",
+            "groups",
+            "user_permissions",
+        ]
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
